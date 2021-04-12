3b897aecf0f636c3870468a21d3f8be8aae24401
Arihant-Sandee <62185899+Arihant-Sandee@users.noreply.github.com>
2021-04-12 23:23:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import MyBag from "./Pages/MyBag";
 import UserProfile from "./Pages/UserProfile";
 import Settings from "./Pages/Settings";
 import Footer from "./Components/Footer";
+import Login, { LoginForm } from "../src/Components/accountBox/loginForm";
+import Login2, { LoginForm2 } from "../src/Components/accountBox/loginForm2";
+import Signup, { SignupForm } from "../src/Components/accountBox/signupForm";
+import Signup2, { SignupForm2 } from "../src/Components/accountBox/signupForm2";
 
 function App() {
   return (
@@ -18,6 +22,10 @@ function App() {
       <Route exact path='/mybag' component={MyBag}/>
       <Route exact path='/userprofile' component={UserProfile}/>
       <Route exact path='/settings' component={Settings}/>
+      <Route exact path='/userlogin' component={LoginForm}/>
+      <Route exact path='/retailerlogin' component={LoginForm2}/>
+      <Route exact path='/usersignup' component={SignupForm}/>
+      <Route exact path='/retailersignup' component={SignupForm2}/>
       <Footer/>
       </BrowserRouter>
     </div>
