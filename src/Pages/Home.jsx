7c23e922f926e4f8b6d20a9931fb2e79bc3cb55a
khushali77 <201801264@daiict.ac.in>
2021-04-12 11:00:45 +0530

    Added: Customer Homepage with carousals

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,73 @@
 import React from "react";
+import Carousel from "react-elastic-carousel";
+import Video2 from "../Components/Video2.mp4";
+import {brands,breakPoints,prod,cat} from "../data";
+import Card1 from "../Components/Card";
+import "../Css/Home.css";
 
 function Home(){
     return(
-        <p>New page</p>
-    )
+        <div className="app__home">
+            
+            <div className="app__main">
+            {/* Search bar */}
+            <div className="search__bar">
+            </div>
+            {/* Video on the main page */}
+            <video className="app__video" autoPlay muted loop>
+                <source src={Video2} type="video/mp4"/>
+            </video>
+            </div>
+            
+            <div className="sections">
+                {/* Shop by category section */}
+                <div className="section__category">
+                    <h2>Shop by Category</h2>
+                    <div className="brand__cat">
+                        <Carousel breakPoints={breakPoints}>
+                        {cat.map((photo)=>{
+                            return( 
+                            <div>
+                                <Card1 img={photo.url} name={photo.name}/>
+                            </div>)
+                            })}
+                        </Carousel>
+                    </div>
+                </div>
+
+                {/* Shop by Product section */}    
+                <div className="section__product">
+                    <h2>Shop by Product</h2>
+                    <div className="brand__prod">
+                    <Carousel breakPoints={breakPoints}>
+                    {prod.map((photo)=>{
+                        return( 
+                        <div>
+                            <Card1 img={photo.url} name={photo.name}/>
+                        </div>)
+                        })}
+                    </Carousel>
+                    </div>
+                </div>
+
+                {/* Shop by Brand section */}    
+                <div className="section__brand">
+                    <h2>Shop by Brand</h2>
+                    <div className="brand__car">
+                    <Carousel breakPoints={breakPoints}>
+                    {brands.map((photo)=>{
+                        return( 
+                        <div>
+                            <Card1 img={photo.url} name={photo.name}/>
+                        </div>)
+                        })}
+                    </Carousel>
+                    </div>
+                </div>
+            </div> 
+            
+        </div>
+    )  
 }
 
 export default Home;
