5533d25252d28539fc39c90b523dea32e22a8873
khushali77 <201801264@daiict.ac.in>
2021-04-13 19:51:14 +0530

    Product Cards added

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import "../Css/Products.css";
 import {Dropdown, DropdownButton} from 'react-bootstrap'
+import {Card} from "react-bootstrap";
+import {prod} from "../data";
+import check from "../Assets/heart.png";
+
+function DisplayCard(props){
+    return(
+        <div className="per__card">
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={props.data.url} className="itemimg"/>
+        <hr size="10"></hr>
+            <Card.Body>
+                <div className="product__details">
+                    <Card.Text className="product__price"> Price : Rs {props.data.price}</Card.Text>
+                <div className="product__details__btn">
+                    <button type = "button" className = "butn">
+                    <i className = "fas fa-shopping-cart icn"></i>Add to cart</button>
+                    <button type = "button" className = "butn"><img src = {check} className = "heart"  alt = "checked"/></button>
+                </div>
+                </div>
+            </Card.Body>
+        </Card>
+        </div>
+    );
+}
 
 function Products(){
     return(
+        <div className="products__page">
+        {/* Top bar */}
         <div className="top__bar">
             <DropdownButton className="filter" id="dropdown-item-button" title="Filter by  " size="md" variant="light">
             <Dropdown.ItemText>Tshirts</Dropdown.ItemText>
@@ -17,6 +43,10 @@ function Products(){
             <Dropdown.Item as="button">Price:high to low</Dropdown.Item>
             <Dropdown.Item as="button">size</Dropdown.Item>
             </DropdownButton>
+        </div>
+        <div className="product__cards">
+            {prod.map(item=>(<li><DisplayCard data={item}/></li>))}
+        </div>
         </div>
     );
 }
