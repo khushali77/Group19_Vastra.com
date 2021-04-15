889beb0c3f68d7483401837c8e5b437664a23758
khushali77 <201801264@daiict.ac.in>
2021-04-15 10:25:39 +0530

    Added: User details section

--- src/Pages/MyBag.jsx
+++ src/Pages/MyBag.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {prod} from "../data.js";
 import "../Css/MyBag.css";
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button'
 
 export const Item = (props) =>{
     return( 
@@ -19,7 +20,7 @@ export const Item = (props) =>{
      );
  }
 
-function MyBag(){
+function MyBag(props){
     return(
         <div className="mybag">
         <h2>It's your Cart :)</h2>
@@ -28,8 +29,29 @@ function MyBag(){
                 {prod.map(item=>(<li key={item.id}><Item data={item}/></li>))}
                 </div>
                 <div className="cart__details">
-                    <p>Details</p>
+                    <h2>Details</h2>
                     <hr/>
+                    <div className="user__details">
+                        <h4 className="shipping__address">Shipping details : This is my address, why you bitch are looking here. Get lost.</h4>
+                        <div className="cost__details">
+                            <h3 className="subtotal">Subtotal</h3>
+                            <h3 className="cost__subtotal"> ₹ 2563.52</h3>
+                            <h3 className="cgst">CGST</h3>
+                            <h3 className="cost__cgst"> ₹ 200.32</h3>
+                            <h3 className="sgst">SGST</h3>
+                            <h3 className="cost__sgst">₹ 210.50</h3>
+                            <h3 className="shippingcharges">Shipping Charges</h3>
+                            <h3 className="cost__ship">₹ 150.00</h3>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="total__sect">
+                        <h3 className="total">Total</h3>
+                        <h3 className="cost__total">₹ 3506.00</h3>
+                    </div>
+                    <div className="checkout__btn">
+                        <Button variant="custom">Proceed to Checkout</Button>
+                    </div>
                 </div>
             </div>
         </div>
